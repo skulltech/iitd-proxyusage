702eb57b5d404c7a251d512e4bc2f92b49bb25b6
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-03-12 15:22:08 +0530

    Format the code according to PEP8

--- checkusage.py
+++ checkusage.py
@@ -6,48 +6,50 @@ from getpass import getpass
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 
-
 client_id = 'C7o6R5kXQxjshCLt1bFkU9YOnIKMbyzN'
 os.environ['REQUESTS_CA_BUNDLE'] = os.getcwd() + '/CCIITD-CA.crt'
 
+
 def proxyusage(username, password):
-	data = {'username': username, 'password': password, 'submit': ''}
-	params = {'response_type': 'code', 'client_id': client_id, 'state': 'xyz'}
+    data = {'username': username, 'password': password, 'submit': ''}
+    params = {'response_type': 'code', 'client_id': client_id, 'state': 'xyz'}
+    headers = {
+        'Referer': 'https://oauth.iitd.ac.in/login.php?response_type=code&client_id={}&state=xyz'.format(client_id),
+    }
 
-	headers = {
-		'Referer': 'https://oauth.iitd.ac.in/login.php?response_type=code&client_id={}&state=xyz'.format(client_id),
-	}
+    session = requests.Session()
 
-	session = requests.Session()
+    r = session.post('https://oauth.iitd.ac.in/authorize.php', data=data, params=params, headers=headers,
+                     allow_redirects=False)
+    redirect = r.headers['Location']
+    try:
+        r = session.get(redirect, allow_redirects=False)
+    except requests.exceptions.MissingSchema:
+        return False
 
-	r = session.post('https://oauth.iitd.ac.in/authorize.php', data=data, params=params, headers=headers, allow_redirects=False)
-	redirect = r.headers['Location']
-	try:
-		r = session.get(redirect, allow_redirects=False)
-	except requests.exceptions.MissingSchema:
-		return False
+    page = session.get('https://track.iitd.ac.in/data_usage.php')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    elems = soup.select('td[align="right"]')[4:]
 
-	page = session.get('https://track.iitd.ac.in/data_usage.php')
-	soup = BeautifulSoup(page.text, 'html.parser')
-	elems = soup.select('td[align="right"]')[4:]
+    return {'yesterday': elems[0].text, 'week': elems[1].text, 'month': elems[2].text, 'year': elems[3].text}
 
-	return {'yesterday': elems[0].text, 'week': elems[1].text, 'month': elems[2].text, 'year': elems[3].text}
 
 def main():
-	username = input('Enter username: ')
-	password = getpass('Enter password: ')
+    username = input('Enter username: ')
+    password = getpass('Enter password: ')
 
-	with warnings.catch_warnings():
-	    warnings.simplefilter("ignore")
-	    usage = proxyusage(username, password)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        usage = proxyusage(username, password)
 
-	if not usage:
-		print('Invalid credentials!')
-		sys.exit()
-	
-	print('\nData Usage for {}\n'.format(username.upper()))
-	print(tabulate([['Yesterday', usage['yesterday']], ['Week', usage['week']], ['Month', usage['month']], ['Year', usage['year']]]))
+    if not usage:
+        print('Invalid credentials!')
+        sys.exit()
+
+    print('\nData Usage for {}\n'.format(username.upper()))
+    print(tabulate([['Yesterday', usage['yesterday']], ['Week', usage['week']], ['Month', usage['month']],
+                    ['Year', usage['year']]]))
 
 
-if __name__=='__main__':
-	main()
+if __name__ == '__main__':
+    main()

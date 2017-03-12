797870f9ac8fb3c502e129520e980d30f426fc82
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-03-12 15:14:50 +0530

    Added handling of wrong credentials

--- checkusage.py
+++ checkusage.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import warnings
+import sys
 from getpass import getpass
 from bs4 import BeautifulSoup
 from tabulate import tabulate
@@ -21,7 +22,10 @@ def proxyusage(username, password):
 
 	r = session.post('https://oauth.iitd.ac.in/authorize.php', data=data, params=params, headers=headers, allow_redirects=False)
 	redirect = r.headers['Location']
-	r = session.get(redirect, allow_redirects=False)
+	try:
+		r = session.get(redirect, allow_redirects=False)
+	except requests.exceptions.MissingSchema:
+		return False
 
 	page = session.get('https://track.iitd.ac.in/data_usage.php')
 	soup = BeautifulSoup(page.text, 'html.parser')
@@ -36,10 +40,14 @@ def main():
 	with warnings.catch_warnings():
 	    warnings.simplefilter("ignore")
 	    usage = proxyusage(username, password)
+
+	if not usage:
+		print('Invalid credentials!')
+		sys.exit()
 	
 	print('\nData Usage for {}\n'.format(username.upper()))
 	print(tabulate([['Yesterday', usage['yesterday']], ['Week', usage['week']], ['Month', usage['month']], ['Year', usage['month']]]))
-	
+
 
 if __name__=='__main__':
 	main()

67a86a8ad537b9bff7f32076e6144ea4212554bf
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-03-12 14:59:33 +0530

    Wrapped the code in functions.

--- checkusage.py
+++ checkusage.py
@@ -3,32 +3,41 @@ import requests
 from getpass import getpass
 from bs4 import BeautifulSoup
 
-client_id = 'C7o6R5kXQxjshCLt1bFkU9YOnIKMbyzN'
 
-username = input('Enter username: ') or 'cs5160400'
-print(username)
-password = getpass('Enter password: ')
+client_id = 'C7o6R5kXQxjshCLt1bFkU9YOnIKMbyzN'
 os.environ['REQUESTS_CA_BUNDLE'] = os.getcwd() + '/CCIITD-CA.crt'
 
-data = {'username': username, 'password': password, 'submit': ''}
-params = {'response_type': 'code', 'client_id': client_id, 'state': 'xyz'}
+def proxyusage(username, password):
+	data = {'username': username, 'password': password, 'submit': ''}
+	params = {'response_type': 'code', 'client_id': client_id, 'state': 'xyz'}
 
-headers = {
-	'Referer': 'https://oauth.iitd.ac.in/login.php?response_type=code&client_id={}&state=xyz'.format(client_id),
-}
+	headers = {
+		'Referer': 'https://oauth.iitd.ac.in/login.php?response_type=code&client_id={}&state=xyz'.format(client_id),
+	}
 
-session = requests.Session()
+	session = requests.Session()
 
-r = session.post('https://oauth.iitd.ac.in/authorize.php', data=data, params=params, headers=headers, allow_redirects=False)
-redirect = r.headers['Location']
-r = session.get(redirect, allow_redirects=False)
+	r = session.post('https://oauth.iitd.ac.in/authorize.php', data=data, params=params, headers=headers, allow_redirects=False)
+	redirect = r.headers['Location']
+	r = session.get(redirect, allow_redirects=False)
 
-page = session.get('https://track.iitd.ac.in/data_usage.php')
-soup = BeautifulSoup(page.text, 'html.parser')
-elems = soup.select('td[align="right"]')[4:]
+	page = session.get('https://track.iitd.ac.in/data_usage.php')
+	soup = BeautifulSoup(page.text, 'html.parser')
+	elems = soup.select('td[align="right"]')[4:]
 
-print('Data Usage for {}\n'.format(username.upper()))
-print('Yesterday: {}'.format(elems[0].text))
-print('Week: {}'.format(elems[1].text))
-print('Month: {}'.format(elems[2].text))
-print('Year: {}'.format(elems[3].text))
+	return {'yesterday': elems[0].text, 'week': elems[1].text, 'month': elems[2].text, 'year': elems[3].text}
+
+def main():
+	username = input('Enter username: ')
+	password = getpass('Enter password: ')
+	usage = proxyusage(username, password)
+
+	print('Data Usage for {}\n'.format(username.upper()))
+	print('Yesterday: {}'.format(usage['yesterday']))
+	print('Week: {}'.format(usage['week']))
+	print('Month: {}'.format(usage['month']))
+	print('Year: {}'.format(usage['year']))
+
+
+if __name__=='__main__':
+	main()

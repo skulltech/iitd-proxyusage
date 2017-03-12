151a1e311247cf2952ab48d40ed28a22a806c0d7
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-03-12 05:45:41 +0530

    Added support for CA cert. Removed implementation of requests-oauthlib.

--- login.py
+++ login.py
@@ -1,14 +1,13 @@
-from oauthlib.oauth2 import LegacyApplicationClient
-from requests_oauthlib import OAuth2Session
 from getpass import getpass
 import requests
-
+import os
 
 client_id = 'C7o6R5kXQxjshCLt1bFkU9YOnIKMbyzN'
 
 username = input('Enter username: ') or 'cs5160400'
 print(username)
 password = getpass('Enter password: ')
+os.environ['REQUESTS_CA_BUNDLE'] = os.getcwd() + '/CCIITD-CA.crt'
 
 data = {'username': username, 'password': password, 'submit': ''}
 params = {'response_type': 'code', 'client_id': client_id, 'state': 'xyz'}
@@ -18,14 +17,9 @@ headers = {
 
 session = requests.Session()
 
-r = session.post('https://oauth.iitd.ac.in/authorize.php', data=data, params=params, headers=headers, verify=False, allow_redirects=False)
+r = session.post('https://oauth.iitd.ac.in/authorize.php', data=data, params=params, headers=headers, allow_redirects=False)
 redirect = r.headers['Location']
-r = session.get(redirect, verify=False, allow_redirects=False)
+r = session.get(redirect, allow_redirects=False)
 
 resp = session.get('https://track.iitd.ac.in/data_usage.php')
 print(resp.text)
-
-
-# oauth = OAuth2Session(client=LegacyApplicationClient(client_id=client_id))
-# token = oauth.fetch_token(token_url='https://oauth.iitd.ac.in/authorize.php', username=username, password=password)
-# print(token)
